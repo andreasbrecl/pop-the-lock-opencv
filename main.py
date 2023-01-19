7ab3617f684f143bc8ef20f501b4ad87433cf478
andreasbrecl <andreas@brecl.com>
2023-01-19 13:20:36 -0700

    created main python file

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+Authors: Andreas Brecl, Nick Aichholz 
+Date: 01/19/2023
+
+This program will use opencv to beat the pop the lock arcade game. This will
+use a raspberry pi and a camera to achieve this goal. The raspberry pi will use
+GPIO for it's pin outputs to the servos. A raspberry pi will also be used because
+it is compatible with python. 
+"""
+
+# Import GPIO
+# TODO: Add GPIO import
+
+# Import open CV
+import cv2
+
+# Import other libraries
+
+
+
+def main():
+    """
+    This excutes the primarity defintions of the main script.
+    
+    Input:  None
+
+    Output: None
+    """
+    # 
+
+if __name__ == "__main__":
+    main()
